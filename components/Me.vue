0d16e994f8e3e0b0ffcba3f6968f17ca6a2b71c4
sinedied <noda@free.fr>
2025-03-18 16:26:36 +0100

    Add Me component

---
+++ components/Me.vue
@@ -0,0 +1,15 @@
+<template>
+  <div position="absolute right-6xl bottom-4xl" flex="~ col"  p="4" float="right" font="size-3" border="rounded-xl" bg="op-30 black">
+    <h3>Yohan Lasorsa</h3>
+    <div class="vertical-middle">
+      Principal Developer Advocate @ <img inline align="sub" src="/images/ms.png" alt="Microsoft Logo" width="14" height="14" />
+    </div>
+    <div>GDE for <devicon-angular/>, OSS builder, Web tinkerer</div>
+    <div>
+      <fa6-brands-bluesky /> <fa6-brands-x-twitter /> <fa6-brands-youtube /> <fa6-brands-dev /> <fa6-brands-github /> @sinedied
+    </div>
+    <div>
+      <fa6-brands-linkedin /> /yohanlasorsa
+    </div>
+  </div>
+</template>
